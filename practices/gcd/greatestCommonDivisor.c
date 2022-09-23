55477e7f1e6ce110ee84d2f9e9bb799ca087db01
ARCHIBALD.CARRION <archibald.carrion@ucr.ac.cr>
2022-09-23 10:12:56 -0600

    added main c code

--- practices/gcd/greatestCommonDivisor.c
+++ practices/gcd/greatestCommonDivisor.c
@@ -1,1 +1,14 @@
 // g++ -g bisTest.cc leap_year.s -o bisTest.out
+
+#include <stdio.h>
+extern "C" int greatestCommonDivisor(int, int);
+
+
+int main() {
+    int result;
+    int num1 = 10;
+    int num2 = 12;
+    result = greatestCommonDivisor(num1, num2);
+    printf("The greatest common divisor of %d and %d is %d", num1, num2, result);
+    return 0;
+}
---
+++ practices/gcd/greatestCommonDivisor.s
@@ -0,0 +1,6 @@
+.text
+.global greatestCommonDivisor
+
+greatestCommonDivisor:
+
+
